d019bc2acb6995bb546f46deea0eede8dab2487f
MarcXX33 <166284860+MarcXX33@users.noreply.github.com>
2025-06-19 11:21:51 +0200

    Add files via upload

--- core.js
+++ core.js
@@ -1,1 +1,5 @@
-// loader logik folgt
+// Echter Bangle.js App Loader – Basisstruktur
+// Hinweis: In echter Nutzung stammt dieser Code aus dem offiziellen Repo:
+// https://github.com/espruino/BangleApps/blob/master/core.js
+
+window.location.href = "https://espruino.github.io/BangleApps/?id=balanceaid";
